dbffcd050ee95463c3f96543084795467d43d466
Oscar Romero <oscalito992@gmail.com>
2025-01-26 17:21:13 -0400

    Colors and theme changed

--- app/src/main/java/com/example/calculatorapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/calculatorapp/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.example.calculatorapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DarkBackground = Color(0xFF17171C)
+val DarkText = Color(0xFFFFFFFF)
+val DarkButtonHighEmphasis = Color(0xFF4B5EFC)
+val DarkButtonMediumEmphasis = Color(0xFF4E505F)
+val DarkButtonLowEmphasis = Color(0xFF2E2F38)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightBackground = Color(0xFFF1F2F3)
+val LightText = Color(0xFF000000)
+val LightButtonHighEmphasis = Color(0xFF4B5EFC)
+val LightButtonMediumEmphasis = Color(0xFFD2D3DA)
+val LightButtonLowEmphasis = Color(0xFFFFFFFF)
--- app/src/main/java/com/example/calculatorapp/ui/theme/screens/HomeScreen.kt
+++ app/src/main/java/com/example/calculatorapp/ui/theme/screens/HomeScreen.kt
@@ -1,10 +1,19 @@
 package com.example.calculatorapp.ui.theme.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculatorapp.ui.theme.CalculatorAppTheme
 
 @Composable
@@ -13,10 +22,10 @@ fun HomeScreen() {
 }
 
 
-@Preview
+@Preview(name = "LightTheme")
 @Composable
-fun HomeScreenPreview() {
-    CalculatorAppTheme {
+fun HomeScreenLightThemePreview() {
+    CalculatorAppTheme() {
         Surface(
             modifier = Modifier.fillMaxSize()
         ) {
@@ -24,3 +33,16 @@ fun HomeScreenPreview() {
         }
     }
 }
+
+@Preview(name = "DarkTheme")
+@Composable
+fun HomeScreenDarkThemePreview() {
+    CalculatorAppTheme(darkTheme = true) {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            HomeScreen()
+        }
+    }
+}
+
